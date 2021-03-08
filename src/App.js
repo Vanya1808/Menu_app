caa89e792e09a2b00d7e4281b75503dba283ae31
Vanya <56477758+Vanya1808@users.noreply.github.com>
2021-03-08 21:55:13 +0300

    not working yet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import React from "react";
+import product from "./data.json";
+import Products from "./components/Products"
 
-function App() {
+
+class App extends React.Component {
+    constructor(){
+      super();
+      this.state = {
+        products: data.products,
+        size: '',
+        sort: "",
+      };
+
+  
+  };
+ 
+
+}
+ 
   return (
     <div className="grid-container">
-    <header>
-      <a href='/'>React Shopping Cart</a>     
-    </header>
-    <main>
-   Product List
-    </main>
-    <footer>
-      All rights are reserved
-    </footer>
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        <div className="content">
+          <div className="main">
+            <Products products={this.state.products}></Products>
+          </div>
+          <div className="sidebar">Cart Items</div>
+
+        </div>
+      </main>
+      <footer>All rights are reserved</footer>
     </div>
-  )
-   
-}
+  );
+
 export default App;
